467960775f8542454b3df12e7396783fb59d4482
Ashwini <ashwini.swe@gmail.com>
2024-04-18 14:46:27 +0530

    Add some style and remove some file

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -542,16 +542,11 @@ function Profile({ item }) {
 
                               {/* TextArea */}
 
-                              <label
-                                for="message"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white pt-4"
-                              >
-                                Your message
-                              </label>
+                            
                               <textarea
                                 id="message"
                                 rows="4"
-                                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pt-4"
                                 placeholder="Include a few brief details about what you did in this role. Try to focus on your most meaningful accomplishments, and use numbers to quantify them where possible."
                               ></textarea>
                               <div className="flex md:flex-row justify-start items-start mt-4 gap-8">
