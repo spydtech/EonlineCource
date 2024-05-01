52650431a3d7820d103e881b5209075208299f33
Ashwini <ashwini.swe@gmail.com>
2024-05-01 12:46:15 +0530

    working on student part

--- src/Components/Home/Plat form/particularCoursesPage/Outcomes.jsx
+++ src/Components/Home/Plat form/particularCoursesPage/Outcomes.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Outcomes = () => {
   return (
-    <div>Outcomes</div>
+    <>
+    
+    <div className='flex flex-row justify-center items-center'>
+      <div className='w-full'>
+      <div className='text-start text-xl font-bold'>Elevate your career with sought-after skills in full stack web development</div>
+        <ul className='list-disc pl-4'>
+          <li>Acquire full stack web skills.Industry-recognized certification. Excel in tech.</li>
+          <li>Master front-end, back-end. Real-world projects. Career-ready in web development.</li>
+          <li>Elevate career with full stack. Google-endorsed. Prepare for industry. Success awaits.</li>
+        </ul>
+      </div>
+      <div className=' w-[750px]'>
+        <img src="https://images.ctfassets.net/wp1lcwdav1p1/2CUbULaq9mEfSSIq6lsCUu/b8ec58abf5106bf9bf75b17da09c39c0/What_is_data_analysis.png?w=1500&h=680&q=60&fit=fill&f=faces&fm=jpg&fl=progressive" className='rounded-xl'/>
+      </div>
+    </div>
+    </>
   )
 }
 
