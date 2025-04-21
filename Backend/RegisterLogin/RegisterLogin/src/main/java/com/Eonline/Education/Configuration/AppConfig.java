209b25f9e08dbe8225e00d720cebfe44e7aeaa5d
spydtech <166598988+spydtech@users.noreply.github.com>
2025-04-21 15:02:32 +0530

    Update AppConfig.java

--- Backend/RegisterLogin/RegisterLogin/src/main/java/com/Eonline/Education/Configuration/AppConfig.java
+++ Backend/RegisterLogin/RegisterLogin/src/main/java/com/Eonline/Education/Configuration/AppConfig.java
@@ -62,6 +62,11 @@ public class AppConfig {
 
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+         cfg.setAllowedHeaders(List.of(
+                "X-Requested-With",
+                "Accept",
+                "Origin"
+        ));
         cfg.setExposedHeaders(List.of("Authorization"));
         cfg.setAllowCredentials(true);
         cfg.setMaxAge(3600L);
